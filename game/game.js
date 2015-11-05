a726dc7ac59e1d749cd9defa1d1fda72376c0188
Herobryan66 <bsd-02@BSD-02s-iMac.local>
2015-11-05 13:37:06 +0800

    Game parts

--- game/game.js
+++ game/game.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
-	$(#playlink).click(function() {
-		$(#playlink).hide();
-		$(#lol).css("background","url('images/bg.jpg')") ;
-	)};
-)};
-    
+
+	$("#playlink").click(function() {
+	  $("#playlink").hide();
+	  $("#lol").css("background","url('images/bg.jpg')") ;
+	});
+
+});
