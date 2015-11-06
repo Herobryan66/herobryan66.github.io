26d2f61fe8ae270ad3cd55f980d09a8af0410884
Herobryan66 <bsd-02@BSD-02s-iMac.local>
2015-11-06 12:03:17 +0800

    new characters

--- game/game.js
+++ game/game.js
@@ -1,115 +1,20 @@
 $(document).ready(function(){
 
-	$("#playlink").click(function() {
-	  $(".place").hide();
-	  $("#lol").css("background","url('stage.png')");
-	  $(".player").show();
-	  $(".player").css("background","url('playre.png')");
-	  $(".foe").show();
-	  $(".foe").css("background","url('enemytank.png')");
-	});
+ $("#playlink").click(function() {
+	$(".place").hide();
+	$("#lol").css("background","url('stage.png')");
+	$("#lol").css("background-size","cover");
+	$(".player").css("display","inline-block");
+	$("#enemyTank").css("display","inline-block");
+ });
+
+ $("#up").click(function() {
+	$(".player").css("top","2px");
+ });
+
+ $("#left").click(function() {
+	$(".player").css("left","900px");
+ });
+
 });
-
-
-var charImage = new Image();
-charImage.ready = false;
-charImage.onload = setAssetReady;
-charImage.src = PATH_CHAR;
- 
-function setAssetReady()
-{
-	this.ready = true;
-}
- 
-//Display Preloading
-ctx.fillRect(0,0,stage.width,stage.height);
-ctx.fillStyle = "#000";
-ctx.fillText(TEXT_PRELOADING, TEXT_PRELOADING_X, TEXT_PRELOADING_Y);
-var preloader = setInterval(preloading, TIME_PER_FRAME);
-
-function preloading()
-{	
-	if (charImage.ready)
-	{
-		clearInterval(preloader);
-		
-		//Initialise game
-		facing = "E"; //N = North, E = East, S = South, W = West
-		isMoving = false;
-		
-		gameloop = setInterval(update, TIME_PER_FRAME);			
-		document.addEventListener("keydown",keyDownHandler, false);	
-		document.addEventListener("keyup",keyUpHandler, false);	
-	}
-}
-
-function keyDownHandler(event)
-{
-	var keyPressed = String.fromCharCode(event.keyCode);
- 
-	if (keyPressed == "W")
-	{		
-		facing = "N";
-		isMoving = true;
-	}
-	else if (keyPressed == "D")
-	{	
-		facing = "E";
-		isMoving = true;		
-	}
-	else if (keyPressed == "S")
-	{	
-		facing = "S";
-		isMoving = true;		
-	}
-	else if (keyPressed == "A")
-	{	
-		facing = "W";
-		isMoving = true;		
-	}
-}
-
-function keyUpHandler(event)
-{
-	var keyPressed = String.fromCharCode(event.keyCode);
-	
-	if ((keyPressed == "W") || (keyPressed == "A") || 
-		(keyPressed == "S") || (keyPressed == "D"))
-	{
-		isMoving = false;
-	}
-}
-
-if (isMoving)
-	{
-		if (facing == "N")
-		{
-			charY -= CHAR_SPEED;
-			currY = IMAGE_START_NORTH_Y;
-		}
-		else if (facing == "E")
-		{
-			charX += CHAR_SPEED;
-			currY = IMAGE_START_EAST_Y;
-		}
-		else if (facing == "S")
-		{
-			charY += CHAR_SPEED;
-			currY = IMAGE_START_SOUTH_Y;
-		}
-		else if (facing == "W")
-		{
-			charX -= CHAR_SPEED;
-			currY = IMAGE_START_WEST_Y;
-		}
-		
-		currX += CHAR_WIDTH;
-		
-		if (currX >= SPRITE_WIDTH)
-			currX = 0;
-	}
-	
-	//Draw Image
-	ctx.drawImage(charImage,currX,currY,CHAR_WIDTH,CHAR_HEIGHT,
-					charX,charY,CHAR_WIDTH,CHAR_HEIGHT);
 
